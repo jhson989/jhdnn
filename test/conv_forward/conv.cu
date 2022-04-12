855432baa7bf3caa5b1d33baee8eba958ef2c752
Janghyun Son <jhson989@gmail.com>
2022-04-12 11:10:29 +0900

    Add test code

--- test/conv_forward/conv.cu
+++ test/conv_forward/conv.cu
@@ -5,13 +5,6 @@
 int main(void) {
 
 
-    // input
-    float* d_input;
-    std::vector<float> input(3*1*128*128, 0);
-    std::generate( input.begin(), input.end(), [](){ return ((std::rand()%101-50)%10); } );
-    cudaErrChk( cudaMalloc(&d_input, sizeof(float)*3*1*128*128) );
-    cudaErrChk( cudaMemcpy(d_input, input.data(), sizeof(float)*3*1*128*128, cudaMemcpyHostToDevice) );
-    
     cudnn_create();
     cuConvFloat conv_cu(
         3, 3, 128, 128,
@@ -27,30 +20,78 @@ int main(void) {
     // filter
     float filter[] = {
         -1, -1, -1, -1, 9, -1, -1, -1, -1,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
         -1, -1, -1, -1, 9, -1, -1, -1, -1,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
+        1, 0, 0, 0, 0, 0, 0, 4, 0,
         -1, -1, -1, -1, 9, -1, -1, -1, -1
     };
+
+
+
+    /*******************************************************************************
+     * Forward
+     ********************************************************************************/ 
+
+    // input
+    float* d_input;
+    std::vector<float> input(3*3*128*128, 0);
+    std::generate( input.begin(), input.end(), [](){ return ( (std::rand()%101-50)/10.0f); } );
+    cudaErrChk( cudaMalloc(&d_input, sizeof(float)*3*3*128*128) );
+    cudaErrChk( cudaMemcpy(d_input, input.data(), sizeof(float)*3*3*128*128, cudaMemcpyHostToDevice) );
+    
     conv_cu.set_weights(filter);
+    conv_cu.forward(d_input);
     conv_jh.set_weights(filter);
+    conv_jh.forward(d_input);
+
+    /*******************************************************************************
+     * Check result
+     ********************************************************************************/ 
+    float* cu_d_result = conv_cu.get_y();
+    float* jh_d_result = conv_jh.get_y();
+
+    std::vector<float> cu_y(3*3*128*128);
+    cudaErrChk( cudaMemcpy(cu_y.data(), cu_d_result, sizeof(float)*3*3*128*128, cudaMemcpyDeviceToHost) );
+
+    std::vector<float> jh_y(3*3*128*128);
+    cudaErrChk( cudaMemcpy(jh_y.data(), jh_d_result, sizeof(float)*3*3*128*128, cudaMemcpyDeviceToHost) );
+    cudaErrChk( cudaDeviceSynchronize() );  
+
+    bool check = true;
+    for (int i=0; i<cu_y.size(); i++) {
+        if( cu_y[i] != jh_y[i] ) {
+            check = false;
+            printf("error!: cu[%d](%.3f) != jh[%d](%.3f)\n", i, cu_y[i], i, jh_y[i]);
+            break;
+        }
+    }
+    if (check) {
+        printf("No error!\n");
+    }
+    
+
+
+    /*******************************************************************************
+     * Backward
+     ********************************************************************************/ 
 
     float* d_dy;
-    std::vector<float> dy(3*1*128*128, 1);
-    cudaErrChk( cudaMalloc(&d_dy, sizeof(float)*3*1*128*128) );
-    cudaErrChk( cudaMemcpy(d_dy, dy.data(), sizeof(float)*3*1*128*128, cudaMemcpyHostToDevice) );
+    std::vector<float> dy(3*3*128*128, 1);
+    cudaErrChk( cudaMalloc(&d_dy, sizeof(float)*3*3*128*128) );
+    cudaErrChk( cudaMemcpy(d_dy, dy.data(), sizeof(float)*3*3*128*128, cudaMemcpyHostToDevice) );
     cudaErrChk( cudaDeviceSynchronize() );
 
-    conv_cu.forward(d_input);
+
     conv_cu.backward(d_dy);
-
-
-    conv_jh.forward(d_input);
     conv_jh.backward(d_dy);
+
+
+
+
 
     cudnn_destroy();
     return 0;       
